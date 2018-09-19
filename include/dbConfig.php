7e93dae3cf0606995d26b4a3dc5a8fb373528106
andrea <eandram@hotmail.it>
2018-09-19 16:50:10 +0100

    first commit

---
+++ include/dbConfig.php
@@ -0,0 +1,47 @@
+<?php
+
+/* --- Internal credential to our database (the TT database in our case) --- */
+
+$driver = 'mysql';
+$dbName = 'tt';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "$driver:host=$host;dbname=$dbName;charset=$charset";
+$options = [
+	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+
+/* --- Connection to our DB, instance name: $pdoTT --- */
+
+try {
+	$pdoTT = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+	throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+
+/* --- Credential for the unknown source DB
+	Uncomment and fill the variables with value different from Internal DB --- */
+
+//$driver = '';
+$dbName = 'source';
+//$host = '';
+//$user = '';
+//$pass = '';
+//$charset = '';
+
+$dsn = "$driver:host=$host;dbname=$dbName;charset=$charset";
+
+
+/* --- Connection to external source DB , instance name: $pdoSource --- */
+
+try {
+	$pdoSource = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $i) {
+	throw new PDOException($i->getMessage(), (int)$i->getCode());
+}
